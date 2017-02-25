1e16ce4bab5ab4865c216553f8d67b9ca5f281ac
forwardmomentum <empirefallstechnologies@gmail.com>
2017-02-25 15:36:27 +0300

    movies template fixed

--- public/config.movie.template.js
+++ public/config.movie.template.js
@@ -46,7 +46,7 @@ module.exports = {
             cardIcon: 'person',   // choose from material icons set
             mainLabelField: 'name',             // mainLabelField and extraMainLabelFields fills card header, mainLabelField is required
             subLabelField: 'birthplace',         // subLabelField fills card subheader, default - undefined
-            searchFields: ['name', 'birthplace'],  // search row will use this fields for
+            searchFields: ['name'],  // search row will use this fields for
             pages: [
                 {
                     title: 'Personal',
@@ -88,8 +88,8 @@ module.exports = {
                 },
             ],
             color: 'brown',
-            searchFields: ['title', 'studio'],
-            icon: 'factory',
+            searchFields: ['title'],
+            nodeIcon: 'factory',
             mainLabelField: 'title',
             pages: [
                 {
