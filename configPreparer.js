52a20234974343bcd51b83ced2ef328e99540bec
forwardmomentum <empirefallstechnologies@gmail.com>
2017-03-08 18:35:04 +0300

    hotfixes

---
+++ configPreparer.js
@@ -0,0 +1,12 @@
+/**
+ * Created by forwardmomentum on 21.02.17.
+ */
+
+module.exports = {
+    prepareNodeTypes (nodeTypes) {
+        return nodeTypes;
+    },
+    prepareEdgeTypes (edgeTypes) {
+        return edgeTypes;
+    }
+};
