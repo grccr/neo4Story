4238e4ed903cecfe1ee876ee2fb35fb0f10b3e58
forwardmomentum <empirefallstechnologies@gmail.com>
2017-03-30 13:42:36 +0300

    neo4 parse response now listens to configuration

--- public/components/search/autocompleteSearch/autocompleteRow.vue
+++ public/components/search/autocompleteSearch/autocompleteRow.vue
@@ -1,7 +1,7 @@
 <template>
         <div class="md-list-item-holder">
-            {{ label }}
-            <div v-if="sublabel" class="additional-label"> {{ sublabel }} </div>
+            {{ information.label }}
+            <div v-if="information.sublabel" class="additional-label"> {{ information.sublabel }} </div>
         </div>
 </template>
 <script>
@@ -16,20 +16,6 @@
                     return type.name == this.information.semantic_type;
                 });
                 return match.length > 0 ? match[0] : false;
-            },
-            label () {
-                var extra = '';
-                if(this.typeConfig.extraMainLabelFields)
-                    this.typeConfig.extraMainLabelFields.forEach((extraField) => {
-                        if (this.information[extraField])
-                            extra += this.information[extraField] + ' ';
-                    });
-                if (extra)  extra = extra.slice(0, -1);
-                return this.information[this.typeConfig.mainLabelField] + ' ' + extra;
-            },
-            sublabel() {
-                if(this.typeConfig.subLabelField) return this.information[this.typeConfig.subLabelField];
-                return '';
             }
         },
         props: ['information'],
