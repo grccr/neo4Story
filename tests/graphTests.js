eda58c620f1e2edccda92c9902b57b8716478dab
Nikita Fedkin <lolkasasa@gmail.com>
2017-04-15 22:03:15 +0300

    edge card done, test done

--- tests/graphTests.js
+++ tests/graphTests.js
@@ -18,15 +18,37 @@ describe('mutations', () => {
     it('ADD_NODE_TO_SELECTION', () => {
         // mock state
         const state = {
-            nodes:[],
+            nodes:[{
+                id: 'test'
+            }],
             edges:[],
             selectedNodes: []
         };
         // apply mutation
-        node =
+        let node = {
+            id: 'test'
+        };
         ADD_NODE_TO_SELECTION(state, node);
         // assert result
-        expect(state.nodes[0].id).to.equal("n3");
+        expect(state.nodes[0].selected).to.equal(true);
+    });
+
+    it('ADD_EDGE_TO_SELECTION', () => {
+        // mock state
+        const state = {
+            nodes:[],
+            edges:[{
+                id: 'test'
+            }],
+            selectedEdges: []
+        };
+        // apply mutation
+        let edge = {
+            id: 'test'
+        };
+        ADD_EDGE_TO_SELECTION(state, edge);
+        // assert result
+        expect(state.edges[0].selected).to.equal(true);
     })
 });
 
